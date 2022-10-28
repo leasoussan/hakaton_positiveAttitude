4dfc9a47a88fef874e296a3464d4e1d7a2897ff2
lea <leampdi@gmail.com>
2022-10-28 13:38:06 +0300

    emoticon setup

---
+++ emotions.js
@@ -0,0 +1,11 @@
+import * as img from 'images'
+
+for (let i=0;i<img.length;i++)
+{
+document.querySelector("emoticonsList").innerHTML+=('<img src='+img[i].src);
+
+}
+
+
+
+
---
+++ script.js
@@ -0,0 +1,18 @@
+// steps of building the project
+// the aim of the page is to give some positive affirmations 
+// Bonus: drag and drop of vision board
+
+
+// Affirmation project
+// HTML Design 
+// I need a place for emojies- 
+// place for drag them and 
+// Place for the sentence to be produced.
+
+// Javascript
+// create emojies- in draggable area 
+// create dropin area - with max  emojies 
+// to the drag and drop of emogies - with max of 5 
+// creat a button to execute condition and sentence ccoring 
+// const emotionList = emotionalIcon.sort()
+// console.log(emotionList)
